e347c7bfef81368efd4174d1654d179b3ac716ae
SleepyGabe <gaboparinas78@gmail.com>
2021-10-21 15:24:02 +0800

    21/10/2021
    Tried to add the cards-styled homepage similar to those in popular e-commerce sites. Still need to figure out changing the color scheme

---
+++ ENTPROG - PPEs Ordering System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ENTPROG___PPEs_Ordering_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
